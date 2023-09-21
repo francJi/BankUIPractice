0ca25cc486854f7d56f9456723f93ad51fcf9afd
francJi <minkyuji95@hanmail.net>
2023-09-21 22:42:32 +0900

    [ADD] Define 스크립트 추가
    enum 으로 각 Scene 이름 저장

---
+++ Assets/Scripts/Util/Define.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Define : MonoBehaviour
+{
+    public enum Scene
+    {
+        MainScene,
+    }
+}
