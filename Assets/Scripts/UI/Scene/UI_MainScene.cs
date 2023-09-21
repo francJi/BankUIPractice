2c921e79887f6ac458e4061974a518d33cafba78
francJi <minkyuji95@hanmail.net>
2023-09-22 02:57:19 +0900

    [UPDATE] MainScene 스크립트 수정

--- Assets/Scripts/UI/Scene/UI_MainScene.cs
+++ Assets/Scripts/UI/Scene/UI_MainScene.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class UI_MainScene : UI_Scene
 {
+    [SerializeField] private GameObject _parentObject;
     enum Texts
     {
         MainNameText,
@@ -16,8 +18,10 @@ public class UI_MainScene : UI_Scene
 
     enum Buttons
     {
-
+        DepositMenuButton,
+        WithDrawMenuButton,
     }
+
 
     private void Start()
     {
@@ -31,6 +35,7 @@ public class UI_MainScene : UI_Scene
         }
         //TODO
         BindText(typeof(Texts));
+        BindButton(typeof(Buttons)); 
 
         GetTextMesh((int)Texts.MainNameText).text = "Sparta Bank\r\nATM";
         GetTextMesh((int)Texts.CashBoxNameText).text = "현금";
@@ -38,6 +43,22 @@ public class UI_MainScene : UI_Scene
         GetTextMesh((int)Texts.AccountNameText).text = "지민규";
         GetTextMesh((int)Texts.AccountBalanceNameText).text = "Balance";
         GetTextMesh((int)Texts.AccountBalanceText).text = "0"; // 추후에 PlayerPrefs에서 잔액 받아와야함
+
+        _parentObject = GetButton((int)Buttons.DepositMenuButton).gameObject.transform.parent.gameObject;
+        GetButton((int)Buttons.DepositMenuButton).gameObject.BindEvent(OnClickedDepositMenuButton);
+        GetButton((int)Buttons.WithDrawMenuButton).gameObject.BindEvent(OnClickedWithdrawMenuButton);
         return true;
+    }
+
+    void OnClickedDepositMenuButton()
+    {
+        _parentObject.SetActive(false);
+        UIManager.UIinstance.ShowPopupUI<UI_DepositPopup>();
+    }
+
+    private void OnClickedWithdrawMenuButton()
+    {
+        _parentObject.SetActive(false);
+        UIManager.UIinstance.ShowPopupUI<UI_WithDrawPopup>();
     }
 }
